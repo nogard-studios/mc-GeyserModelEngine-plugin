468757762f3a6f2d74c868f3c8e5016db0b345a5
xSquishyLiam <56977759+xSquishyLiam@users.noreply.github.com>
2025-11-02 16:46:32 +0000

    updated BetterModel API?

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,11 +20,11 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.10-R0.1-SNAPSHOT")
     implementation("dev.jorel:commandapi-paper-shade:11.0.0")
 
     compileOnly("com.ticxo.modelengine:ModelEngine:R4.0.9")
-    compileOnly("io.github.toxicity188:bettermodel:1.13.4")
+    compileOnly("io.github.toxicity188:bettermodel:1.14.0")
 
     compileOnly(files("libs/geyserutils-spigot-1.0-SNAPSHOT.jar"))
     compileOnly("org.geysermc.floodgate:api:2.2.4-SNAPSHOT")
